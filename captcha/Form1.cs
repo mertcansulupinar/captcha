4132173710006ac21585a6a93a55f63fbe72a57f
Mert Can Sulupinar <mertcansulupinar@gmail.com>
2019-12-24 15:31:39 +0300

    Add project files.

---
+++ captcha/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace captcha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string[] symbol1 = { "a", "b", "c", "k", "d", "y" };
+            string[] symbol2 = { "+", "-", "£", "$", "*" };
+            int[] symbol3 = { 3, 5, 7, 9, 6, 1, 4 };
+            char[] symbol4 = { 'v', 'p', 't', 'i', 'r' };
+
+
+            Random r = new Random();
+            int s1, s2, s3, s4, s5;
+            s1 = r.Next(0, symbol1.Length);
+            s2 = r.Next(0, symbol2.Length);
+            s3 = r.Next(0, symbol1.Length);
+            s4 = r.Next(0, symbol3.Length);
+            s5 = r.Next(0, symbol4.Length);
+
+            label1.Text = symbol1[s1].ToString() + symbol2[s2].ToString() + symbol1[s3].ToUpper().ToString() + symbol3[s4].ToString() + symbol4[s5].ToString();
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == textBox1.Text)
+            {
+                Form2 fr = new Form2();
+                fr.Show();
+                //this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı Giriş");
+            }
+        }
+    }
+}
---
+++ captcha/Form2.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace captcha
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 180);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Uygulamayı Kapat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(506, 309);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Uygulama";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
